0333c30f11f114a9a2fafa3279b1c2671673145c
Ertuğrul Sarıhan <74004586+ertugrulsarihan@users.noreply.github.com>
2021-05-27 17:55:59 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  console.log("Selamın aleyküm");
+  
   return (
     <div>
       <h2>{new Date().toDateString()}</h2>
-      <div>selam gençlik</div>
+      
     </div>
   );
 }
